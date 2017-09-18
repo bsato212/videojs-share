d73a630c3f38279398e011b34aa62cc926eb4d17
Bruno Borsato <brunoborsato@outlook.com>
2017-09-18 17:48:24 -0300

    No embed code

--- src/ShareModalContent.js
+++ src/ShareModalContent.js
@@ -93,14 +93,6 @@ export default class ShareModalContent {
             ${copyBtn}
           </div>
         </div>
-
-        <div class="vjs-share__subtitle hidden-xs">${this.player.localize('Embed Code')}:</div>
-        <div class="vjs-share__short-link-wrapper hidden-xs">
-          <input class="vjs-share__short-link" type="text" readonly="true" value="${this.options.embedCode}">
-          <div class="vjs-share__btn">
-            ${copyBtn}
-          </div>
-        </div>
       </div>
 
       <div class="vjs-share__bottom">
